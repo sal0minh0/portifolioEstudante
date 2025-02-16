96ba1019af28b534c07c596d58dbec6ff4bde2e5
Salominho <salomaomoraes1@hotmail.com>
2025-02-16 13:13:53 -0300

    colocando um alert bonito

---
+++ scripts/alerta.js
@@ -0,0 +1,13 @@
+function enviarMensagem() {
+
+    Swal.fire({
+        title: "Seu E-mail foi enviado!",
+        color: "rgba(0, 0, 0, 0.8)",
+        icon: "success",
+        iconColor: "rgba(50, 205, 50, 0.9)",
+        confirmButtonColor: "rgba(128, 0, 0, 0.9)",
+        backdrop: "rgba(0, 0, 0, 0.8)",
+        heightAuto: true,
+        widthAuto: true
+      });
+}
