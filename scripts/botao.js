3d843b2e2866defc315dd1b547abdf5048aa300f
Salominho <salomaomoraes1@hotmail.com>
2025-02-16 13:13:39 -0300

    ajeitando o botao responsivo

---
+++ scripts/botao.js
@@ -0,0 +1,33 @@
+// Transformar o menu em um menu responsivo
+// Armazenar a classe que gerencia o botao
+const menuResponsivo = document.querySelector('.menu-responsive');
+
+// Quando algo acontecer com essa variável adicionar um evento para essa variável
+menuResponsivo.addEventListener('click', () => {
+    mudarMenu();
+});
+
+function mudarMenu() {
+    // Armazenar a classe que gerencia a barra nav responsiva
+    const nav = document.querySelector('.nav-responsive');
+
+    // Se for apertado o botão vai colocar ou retirar a classe 'change'
+    menuResponsivo.classList.toggle('change');
+
+    // Fazer uma validação se tiver a change vai colocar o display block, se não vai ser none
+
+    if (menuResponsivo.classList.contains('change')) {
+        nav.style.display = 'block';
+    
+    } else {
+        nav.style.display = 'none';
+    }
+
+    document.querySelectorAll('.nav-responsive a').forEach(link => {
+        link.addEventListener('click', () => {
+            menuResponsivo.classList.remove('change');
+            nav.style.display = 'none';
+        });
+    });
+    
+}
