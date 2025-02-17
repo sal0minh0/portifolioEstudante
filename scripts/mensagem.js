cbf711ba8496b7aaec20bf2382a9cd46f6b4017d
Salominho <salomaomoraes1@hotmail.com>
2025-02-17 11:57:24 -0300

    customizando meu sweetalert

--- scripts/mensagem.js
+++ scripts/mensagem.js
@@ -11,15 +11,31 @@ function enviarMensagem(event) {
     form.submit();
     
     // Mostra o SweetAlert2
+
+    let timerInterval;
     Swal.fire({
-      title: "Seu E-mail foi enviado!",
+      title: "<strong>Seu E-mail foi enviado!</strong>",
+      html: 'Clique no "Go back" para voltar',
       color: "rgba(0, 0, 0, 0.8)",
       icon: "success",
       iconColor: "rgba(50, 205, 50, 0.9)",
       confirmButtonColor: "rgba(128, 0, 0, 0.9)",
       backdrop: "rgba(0, 0, 0, 0.8)",
-      heightAuto: true,
+      padding: "0 0 1.5em 0",
+      heightAuto: false,
       width: "28em",
+      timer: 1000,
+      timerProgressBar: true,
+      didOpen: () => {
+        Swal.showLoading();
+        const timer = Swal.getPopup().querySelector("b");
+        timerInterval = setInterval(() => {
+          timer.textContent = `${Swal.getTimerLeft()}`;
+        }, 100);
+      },
+      willClose: () => {
+        clearInterval(timerInterval);
+      }
     });
   }
 }
